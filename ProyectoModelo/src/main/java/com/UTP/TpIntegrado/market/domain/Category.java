b0cd34d4175296196c6152599bb2fd02aeecff77
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2026-05-22 12:51:42 -0500

    NUEVO CAmbios

--- ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/domain/Category.java
+++ ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/domain/Category.java
@@ -1,11 +1,20 @@
 package com.UTP.TpIntegrado.market.domain;
 
+import com.UTP.TpIntegrado.market.persistance.entity.Producto;
+
+import java.util.List;
+
 public class Category {
 
 
     private int categoryId;
     private String category;
     private boolean active;
+
+
+
+
+
 
     public int getCategoryId() {
         return categoryId;
@@ -22,4 +31,15 @@ public class Category {
     public void setCategory(String category) {
         this.category = category;
     }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    //FALTO AGREGAR ESTO
+
 }
--- ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/domain/Product.java
+++ ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/domain/Product.java
@@ -64,4 +64,6 @@ public class Product {
     public void setCategory(Category category) {
         this.category = category;
     }
+
+
 }
