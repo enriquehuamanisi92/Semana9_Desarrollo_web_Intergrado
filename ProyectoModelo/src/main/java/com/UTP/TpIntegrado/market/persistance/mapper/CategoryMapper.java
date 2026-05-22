66aff5e928e1e4b01c451f3101e3b4cfb50db230
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2026-05-22 12:54:34 -0500

    actualizaR V3

--- ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/persistance/mapper/CategoryMapper.java
+++ ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/persistance/mapper/CategoryMapper.java
@@ -16,6 +16,8 @@ public interface CategoryMapper {
             @Mapping(source = "idCategoria", target = "categoryId"),
             @Mapping(source = "descripcion", target = "category"),
             @Mapping(source = "estado", target = "active")
+
+
     })
     Category toCategory(Categoria categoria);
 
