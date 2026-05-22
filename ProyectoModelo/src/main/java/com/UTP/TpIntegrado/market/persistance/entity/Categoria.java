754a43df22a661935a1eef9c8fe764551b73a81a
enriquehuamanisi92 <enriquehuamanisi@gmail.com>
2026-05-22 12:57:41 -0500

    actuzalicion v4

--- ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/persistance/entity/Categoria.java
+++ ProyectoModelo/src/main/java/com/UTP/TpIntegrado/market/persistance/entity/Categoria.java
@@ -44,4 +44,14 @@ public class Categoria {
     public void setEstado(Boolean estado) {
         this.estado = estado;
     }
+
+    //FALTO AGREGAR ESTO
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
